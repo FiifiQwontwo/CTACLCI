af2654df81daf0fbc9a49100d0373236a522e34b
fiifiQwontwo <quophi555@gmail.com>
2021-07-20 15:03:03 -0700

    breadcrumbs for navigation pastor

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -9,7 +9,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mews/', admin.site.urls),
     path('', include('ctac.urls')),
     path('account/', include('account.urls')),
     # path('sentry-debug/', trigger_error),
