271b76e90b104c0744f681879d6ae8c2553f52bd
Ahwireng Michael <43513906+FiifiQwontwo@users.noreply.github.com>
2021-09-28 16:19:39 +0000

    member form not saving html form has issues needed tobe corrected

--- users/utils.py
+++ users/utils.py
@@ -2,7 +2,7 @@ import os
 from twilio.rest import Client
 
 account_sid = 'AC9d1a78bae11c1fbd7f948bf4f1db8447'
-auth_token = 'f5cc6bf6d116231e35bec177d7925a63'
+auth_token = '185620c7d6120dba595db9f6d22fa7e4'
 client = Client(account_sid, auth_token)
 
 
